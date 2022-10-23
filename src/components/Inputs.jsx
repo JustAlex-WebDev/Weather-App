3ff4102441f477823ab05db4b6f98320ba832c07
Alexandar Valov <alexvulov123@gmail.com>
2022-10-23 21:01:13 +0300

    Completely changed the design of the app and made it fully responsive this time.

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -4,36 +4,21 @@ import { HiOutlineLocationMarker } from "react-icons/hi";
 
 const Inputs = () => {
   return (
-    <div className="flex flex-row justify-center my-6 mx-8 sm:mx-16 gap-4">
-      <div className="flex flex-row w-3/4 items-center justify-start gap-2">
-        <form className="relative">
-          <AiOutlineSearch className="absolute top-0 left-0 text-xl rounded-2xl cursor-pointer mt-2 ml-4 text-[#161616]" />
-          <input
-            className="bg-[#fff] w-full text-[#161616] pl-12 py-2 rounded-2xl shadow-lg outline-none font-light capitalize"
-            type="text"
-            placeholder="Search"
-          />
-        </form>
+    <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+      <h1 className="text-2xl font-bold my-2 text-[#fff]">Search A City</h1>
+      <form className="relative mr-8">
+        <AiOutlineSearch className="absolute top-0 left-0 text-xl rounded-2xl cursor-pointer mt-2 ml-4 text-primary" />
+        <input
+          className="w-full bg-secondary placeholder-primary text-primary pl-12 py-2 rounded-2xl shadow-lg outline-none capitalize"
+          type="text"
+          placeholder="Search"
+        />
         <HiOutlineLocationMarker
           size={22}
-          className="text-[#fff] min-w-[1.5rem] cursor-pointer rounded-2xl text-xl transition ease-in-out hover:scale-110 opacity-90 hover:opacity-100"
+          title="Use My Location"
+          className="text-[#fff] absolute top-0 -right-8 mt-2 ml-4 cursor-pointer rounded-2xl hover:opacity-50"
         />
-      </div>
-      <div className="flex flex-row w-1/4 items-center justify-end gap-2">
-        <button
-          name="metric"
-          className="text-base sm:text-lg text-[#fff] opacity-80 hover:opacity-100 font-semibold transition ease-in-out hover:scale-110"
-        >
-          °C
-        </button>
-        <span className="text-xl text-[#fff] opacity-70">/</span>
-        <button
-          name="imperial"
-          className="text-base sm:text-lg text-[#fff] opacity-80 hover:opacity-100 font-semibold transition ease-in-out hover:scale-110"
-        >
-          °F
-        </button>
-      </div>
+      </form>
     </div>
   );
 };
--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -5,33 +5,29 @@ import { MdOutlineWaterDrop } from "react-icons/md";
 
 const TemperatureAndDetails = () => {
   return (
-    <div>
-      <div className="flex items-center justify-center py-6 text-xl text-blue-500">
-        <p>Rain</p>
+    <div className="pb-6">
+      <div className="flex items-center justify-center py-6 text-xl font-semibold text-[#fff]">
+        <p>Sunny</p>
       </div>
-
-      <div className="flex flex-row items-center justify-center gap-16 text-[#fff] py-3">
+      <div className="flex flex-col xxsm:flex-row items-center justify-center gap-8 xxsm:gap-16 text-[#fff]">
         <img
           src="http://openweathermap.org/img/wn/01d@2x.png"
           alt=""
           className="w-20"
         />
-        <p className="text-5xl">34°</p>
+        <p className="text-5xl font-semibold">34°</p>
         <div className="flex flex-col space-y-2">
-          <div className="flex font-light text-sm items-center justify-center">
-            <TbTemperature size={18} className="mr-1" />
-            Real feel:
-            <span className="font-medium ml-1">32°</span>
+          <div className="flex text-sm items-center justify-center">
+            <TbTemperature size={22} className="mr-1" />
+            Real feel: <span className="ml-1">32°</span>
           </div>
-          <div className="flex font-light text-sm items-center justify-center">
-            <MdOutlineWaterDrop size={18} className="mr-1" />
-            Humidity:
-            <span className="font-medium ml-1">43%</span>
+          <div className="flex text-sm items-center justify-center">
+            <MdOutlineWaterDrop size={22} className="mr-1" />
+            Humidity: <span className="ml-1">32°</span>
           </div>
-          <div className="flex font-light text-sm items-center justify-center">
-            <RiWindyFill size={18} className="mr-1" />
-            Wind:
-            <span className="font-medium ml-1">3 km/h</span>
+          <div className="flex text-sm items-center justify-center">
+            <RiWindyFill size={22} className="mr-1" />
+            Wind: <span className="ml-1">3 km/h</span>
           </div>
         </div>
       </div>
